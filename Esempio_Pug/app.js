1b0f307d04068248f7773dc6e513ee429ac58ba1
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2019-10-25 10:52:57 +0000

    4

--- Esempio_Pug/app.js
+++ Esempio_Pug/app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
 
 app.get('/', function (req, res) {
-   res.render('index');
+  res.render('index', { //oggetto JSON
+   title: 'Homepage',
+   content : 'Questa pagina parla del mondo e di tanto altro',
+   people: people.profiles //Passa il vettore profiles alla pagina index.pug
+
+ });
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
